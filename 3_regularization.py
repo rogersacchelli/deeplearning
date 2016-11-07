571b216bfbdea45e5cb34bf1d4352c1247e8d370
roger <roger.sacchelli@gmail.com>
2016-11-07 00:23:55 -0200

    Merge branch 'master' of https://github.com/rogersacchelli/deeplearning
    Conflicts:
    	2_fullyconnected.py

--- 3_regularization.py
+++ 3_regularization.py
@@ -35,6 +35,12 @@ print('Training set', train_dataset.shape, train_labels.shape)
 print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
 
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
+batch_size = 128
+
 '''
 
 Problem 1
@@ -43,6 +49,77 @@ Remember that L2 amounts to adding a penalty on the norm of the weights to the l
 In TensorFlow, you can compute the L2 loss for a tensor t using nn.l2_loss(t).
 The right amount of regularization should improve your validation / test accuracy.
 '''
+
+NODES = 1024
+train_subset = 10000
+beta = 0.005
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+    tf_train_dataset = tf.placeholder(tf.float32,
+                                      shape=(batch_size, image_size * image_size))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_valid_dataset = tf.constant(valid_dataset)
+    tf_test_dataset = tf.constant(test_dataset)
+
+    weights_relu = tf.Variable(
+        tf.truncated_normal([image_size * image_size, NODES]))
+    biases_relu = tf.Variable(tf.zeros([NODES]))
+    biases = tf.Variable(tf.zeros([num_labels]))
+
+    print ('shapes w: %s' % str(weights_relu.get_shape()))
+    print ('shapes b: %s' % str(biases_relu.get_shape()))
+
+    relu_train = tf.nn.relu(tf.matmul(tf_train_dataset,weights_relu) + biases_relu)
+    relu_train = tf.nn.dropout(relu_train,0.9)
+    relu_valid = tf.nn.relu(tf.matmul(tf_valid_dataset,weights_relu) + biases_relu)
+    relu_test = tf.nn.relu(tf.matmul(tf_test_dataset,weights_relu) + biases_relu)
+
+    weights_out = tf.Variable(
+        tf.truncated_normal([NODES,num_labels ]))
+
+    logits_train = tf.matmul(relu_train,weights_out) + biases
+    logits_valid = tf.matmul(relu_valid,weights_out) + biases
+    logits_test = tf.matmul(relu_test,weights_out) + biases
+
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits_train, tf_train_labels))
+
+    loss = loss + (beta/2)*(tf.nn.l2_loss(weights_relu)+tf.nn.l2_loss(weights_out))
+
+    optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+    train_prediction = tf.nn.softmax(logits_train)
+    valid_prediction = tf.nn.softmax(logits_valid)
+    test_prediction = tf.nn.softmax(logits_test)
+
+
+num_steps = 5001
+
+with tf.Session(graph=graph) as session:
+  tf.initialize_all_variables().run()
+  print("Initialized")
+  for step in range(num_steps):
+    # Pick an offset within the training data, which has been randomized.
+    # Note: we could use better randomization across epochs.
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    # Generate a minibatch.
+    batch_data = train_dataset[offset:(offset + batch_size), :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+    # Prepare a dictionary telling the session where to feed the minibatch.
+    # The key of the dictionary is the placeholder node of the graph to be fed,
+    # and the value is the numpy array to feed to it.
+    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+    _, l, predictions = session.run(
+      [optimizer, loss, train_prediction], feed_dict=feed_dict)
+    if (step % 500 == 0):
+      print("Minibatch loss at step %d: %f" % (step, l))
+      print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+      print("Validation accuracy: %.1f%%" % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
 
 
 
@@ -72,5 +149,4 @@ Another one is to use learning rate decay:
 global_step = tf.Variable(0)  # count the number of steps taken.
 learning_rate = tf.train.exponential_decay(0.5, global_step, ...)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-
 '''
